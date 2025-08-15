d056e4b3b803c8caf06f3daa97a030e084b99fc9
kreker36barabashka <tackabiog@hotmail.com>
2025-08-14 23:21:53 -0500

    feat: initial commit for frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hệ Thống Quản Lý Thực Phẩm</h1>
+      <p>Chào mừng đến với ứng dụng của chúng tôi!</p>
+    </div>
+  );
+}
+
+export default App;
